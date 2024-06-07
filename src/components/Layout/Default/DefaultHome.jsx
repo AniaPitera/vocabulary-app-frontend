f9b297f7467fed30710acfbdc1482f093bfaabfb
Anna Pitera <annapitera01@gmail.com>
2024-06-07 12:06:05 +0200

    create home pages for logged-in and guest users

---
+++ src/components/Layout/Default/DefaultHome.jsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+
+const Home = () => {
+  return (
+    <Box>
+      <div>
+        <h1>Witaj w VocabularyApp</h1>
+        <p>
+          Zacznij naukę słówek poprzez tworzenie własnych zestawów fiszek do
+          nauki.
+        </p>
+      </div>
+    </Box>
+  );
+};
+
+export default Home;
---
+++ src/components/Layout/User/UserHome.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+const Home = () => {
+  return (
+    <Box>
+      <div>
+        <h1>Witaj w VocabularyApp</h1>
+        <p>Strona główna po zalogowaniu</p>
+      </div>
+    </Box>
+  );
+};
+
+export default Home;
