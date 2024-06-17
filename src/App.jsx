5ca4e0fb008ec67dfd2a3dc136c562d9cb7af42d
Anna Pitera <annapitera01@gmail.com>
2024-06-17 20:05:35 +0200

    update routes & add dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,39 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import DefaultHome from "./components/Layout/Default/DefaultHome";
+import CategoryList from "./components/Category/CategoryList";
 import Footer from "./components/Layout/Footer";
 import UserLayout from "./components/Layout/User/UserLayout";
 import DefaultLayout from "./components/Layout/Default/DefaultLayout";
 import LoginLayout from "./components/Layout/Login/LoginLayout";
 import HomeUser from "./components/Layout/User/UserHome";
+import AddCategory from "./components/Category/AddCategory";
+import { AuthProvider } from "./components/Auth/AuthContext";
+import WordList from "./components/Word/WordList";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<LoginLayout />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Route>
+      <AuthProvider>
+        <Routes>
+          <Route element={<LoginLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
 
-        <Route element={<UserLayout />}>
-          <Route path="/home" element={<HomeUser />} />
-        </Route>
+          <Route element={<UserLayout />}>
+            <Route path="/home" element={<HomeUser />} />
+            <Route path="/categories" element={<CategoryList />} />
+            <Route path="/categories/add" element={<AddCategory />} />
+            <Route path="/categories/:id" element={<WordList />} />
+          </Route>
 
-        <Route element={<DefaultLayout />}>
-          <Route path="/" element={<DefaultHome />} />
-        </Route>
-      </Routes>
-      <Footer />
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<DefaultHome />} />
+          </Route>
+        </Routes>
+        <Footer />
+      </AuthProvider>
     </BrowserRouter>
   );
 };
