22161747a18aa47ffc3a03f13b08ed85b2285a37
Anna Pitera <annapitera01@gmail.com>
2024-06-21 14:59:20 +0200

    add jwt

--- src/components/Auth/AuthContext.jsx
+++ src/components/Auth/AuthContext.jsx
@@ -1,33 +1,34 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    !!localStorage.getItem("token")
-  );
+  const [authToken, setAuthToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState(authToken ? jwtDecode(authToken) : null);
+
   const navigate = useNavigate();
 
   const login = (token) => {
     localStorage.setItem("token", token);
-    setIsAuthenticated(true);
+    setAuthToken(token);
+    setUser(jwtDecode(token));
     navigate("/home");
   };
 
   const logout = () => {
     localStorage.removeItem("token");
-    setIsAuthenticated(false);
+    setAuthToken(null);
+    setUser(null);
     navigate("/login");
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ authToken, user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
 };
 
-export const useAuth = () => {
-  return useContext(AuthContext);
-};
+export default AuthContext;
