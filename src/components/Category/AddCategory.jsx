05b245a9f7b44f6aa9b4b3ddfe71a87f904603eb
Anna Pitera <annapitera01@gmail.com>
2024-06-21 16:12:32 +0200

    refactor AddCategory & CategoryList

--- src/components/Category/AddCategory.jsx
+++ src/components/Category/AddCategory.jsx
@@ -10,14 +10,18 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import axios from "axios";
-
-const API_URL = 'http://localhost:8080/API';
+import { jwtDecode } from "jwt-decode";
 
 export default function AddCategory({ open, handleClose }) {
   const [name, setName] = useState("");
   const [wordLanguage, setWordLanguage] = useState("");
   const [translationLanguage, setTranslationLanguage] = useState("");
-  
+
+  const token = localStorage.getItem("token");
+
+  const decodedToken = token ? jwtDecode(token) : null;
+  const userId = decodedToken ? decodedToken.sub : null;
+
   const handleWordLanguageChange = (event) => {
     setWordLanguage(event.target.value);
   };
@@ -26,79 +30,90 @@ export default function AddCategory({ open, handleClose }) {
     setTranslationLanguage(event.target.value);
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(
+        "/category",
+        {
+          name,
+          wordLanguage,
+          translationLanguage,
+          user: userId,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      handleClose();
+    } catch (error) {
+      console.error("Error creating category:", error);
+    }
+  };
+
   return (
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          component: "form",
-          onSubmit: async (e) => {
-            e.preventDefault();
-            try {
-              await axios.post(`${API_URL}/categories`, { 
-                  name,
-                  wordLanguage,
-                  translationLanguage,
-                });
-              } catch (error) {
-                console.error(error);
-              }
-            handleClose();
-          },
-          sx: {
-            width: "500px", 
-            height: "350px", 
-          },
-        }}
-      >
-        <DialogTitle>Utwórz nowy zestaw słówek</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="name"
-            onChange={(e) => setName(e.target.value)}
-            name="category"
-            label="Podaj nazwę zestawu słówek"
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      PaperProps={{
+        component: "form",
+        onSubmit: handleSubmit,
+        sx: {
+          width: "500px",
+          height: "350px",
+        },
+      }}
+    >
+      <DialogTitle>Utwórz nowy zestaw słówek</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          required
+          margin="dense"
+          id="name"
+          onChange={(e) => setName(e.target.value)}
+          name="category"
+          label="Podaj nazwę zestawu słówek"
+          fullWidth
+        />
+        <FormControl fullWidth margin="dense" required>
+          <InputLabel id="word-language-label">
+            Podaj w jakim języku będą wpowadzane słowa
+          </InputLabel>
+          <Select
+            labelId="word-language-label"
+            id="word-language"
+            value={wordLanguage}
+            onChange={handleWordLanguageChange}
+            label="Podaj w jakim języku będą wpowadzane słowa"
             fullWidth
-          />
-          <FormControl fullWidth margin="dense" required>
-            <InputLabel id="word-language-label">
-              Podaj w jakim języku będą wpowadzane słowa
-            </InputLabel>
-            <Select
-              labelId="word-language-label"
-              id="word-language"
-              value={wordLanguage}
-              onChange={handleWordLanguageChange}
-              label="Podaj w jakim języku będą wpowadzane słowa"
-              fullWidth
-            >
-              <MenuItem value="polski">polski</MenuItem>
-              <MenuItem value="angielski">angielski</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl fullWidth margin="dense" required>
-            <InputLabel id="translation-language-label">
-              Podaj w jakim języku będą tłumaczenia słów
-            </InputLabel>
-            <Select
-              labelId="translation-language-label"
-              id="translation-language"
-              value={translationLanguage}
-              onChange={handleTranslationLanguageChange}
-              label="Podaj w jakim języku będą tłumaczenia słów"
-            >
-              <MenuItem value="polski">polski</MenuItem>
-              <MenuItem value="angielski">angielski</MenuItem>
-            </Select>
-          </FormControl>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Anuluj</Button>
-          <Button type="submit">Utwórz zestaw</Button>
-        </DialogActions>
-      </Dialog>
+          >
+            <MenuItem value="polski">polski</MenuItem>
+            <MenuItem value="angielski">angielski</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl fullWidth margin="dense" required>
+          <InputLabel id="translation-language-label">
+            Podaj w jakim języku będą tłumaczenia słów
+          </InputLabel>
+          <Select
+            labelId="translation-language-label"
+            id="translation-language"
+            value={translationLanguage}
+            onChange={handleTranslationLanguageChange}
+            label="Podaj w jakim języku będą tłumaczenia słów"
+          >
+            <MenuItem value="polski">polski</MenuItem>
+            <MenuItem value="angielski">angielski</MenuItem>
+          </Select>
+        </FormControl>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Anuluj</Button>
+        <Button type="submit">Utwórz zestaw</Button>
+      </DialogActions>
+    </Dialog>
   );
 }
--- src/components/Category/CategoryList.jsx
+++ src/components/Category/CategoryList.jsx
@@ -1,39 +1,92 @@
-import * as React from "react";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardActions from "@mui/material/CardActions";
-import IconButton from "@mui/material/IconButton";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Typography,
+  Card,
+  CardHeader,
+  CardActions,
+  IconButton,
+  Menu,
+  MenuItem,
+  Button,
+  Box,
+} from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { useState } from "react";
-import { Box, Button, Menu, MenuItem, Typography } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import AddCategory from "./AddCategory";
-import { useNavigate } from "react-router-dom";
 
 export default function CategoryList() {
   const [open, setOpen] = React.useState(false);
   const navigate = useNavigate();
-
+  const [categories, setCategories] = useState([]);
   const [anchorEl, setAnchorEl] = useState(null);
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const fetchCategories = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch("/category", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.json();
+      setCategories(data);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
+
+  const handleEdit = async (categoryId, updatedCategory) => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch(`/category/${categoryId}`, {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedCategory),
+      });
+      if (response.ok) {
+        fetchCategories();
+      } else {
+        console.error("Error editing category");
+      }
+    } catch (error) {
+      console.error("Error editing category:", error);
+    }
+  };
+
+  const handleDelete = async (categoryId) => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch(`/category/${categoryId}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        fetchCategories();
+      } else {
+        console.error("Error deleting category");
+      }
+    } catch (error) {
+      console.error("Error deleting category:", error);
+    }
+  };
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
   const handleCloseMenu = () => {
     setAnchorEl(null);
-  };
-
-  const handleEdit = () => {
-    handleClose();
-  };
-
-  const handleDelete = () => {
-    handleClose();
   };
 
   const handleAddClickOpen = () => {
@@ -45,13 +98,11 @@ export default function CategoryList() {
     setOpen(false);
   };
 
-  const handleStartLearning = () => {
-    const categoryId = "123"; 
+  const handleStartLearning = (categoryId) => {
     navigate(`/categories/${categoryId}/learn`);
   };
 
-  const handleCardClick = () => {
-    const categoryId = "123";
+  const handleCategoryDetailClick = (categoryId) => {
     navigate(`/categories/${categoryId}`);
   };
 
@@ -60,42 +111,58 @@ export default function CategoryList() {
       <Typography variant="h4" sx={{ marginTop: 10, textAlign: "center" }}>
         Moje zestawy słówek
       </Typography>
-      <Card sx={{ marginTop: 10 }}>
-        <CardHeader
-          title="Nazwa zestawu"
-          subheader="Język: polski-angielski"
-          action={
-            <>
-              <IconButton aria-label="settings" onClick={handleClick}>
-                <MoreVertIcon />
-              </IconButton>
+      {categories.map((category) => (
+        <Card key={category.id} sx={{ marginTop: 10 }}>
+          <CardHeader
+            title={category.name}
+            subheader={`Język: ${category.word_language || "nieznany"}-${
+              category.translation_language || "nieznany"
+            }`}
+            action={
+              <>
+                <IconButton aria-label="settings" onClick={handleClick}>
+                  <MoreVertIcon />
+                </IconButton>
 
-              <Menu
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={handleCloseMenu}
-              >
-                <MenuItem onClick={handleEdit}>Edytuj </MenuItem>
-                <MenuItem onClick={handleDelete}>Usuń </MenuItem>
-              </Menu>
-            </>
-          }
-        />
-        <CardActions disableSpacing sx={{ ml: 1, mb: 1 }}>
-          <Button
-            size="large"
-            variant="outlined"
-            onClick={handleCardClick}
-            sx={{ mr: 2 }}
-          >
-            Przejdź do zestawu
-          </Button>
+                <Menu
+                  anchorEl={anchorEl}
+                  open={Boolean(anchorEl)}
+                  onClose={handleCloseMenu}
+                >
+                  <MenuItem
+                    onClick={() =>
+                      handleEdit(category.id, { name: "New Category Name" })
+                    }
+                  >
+                    Edytuj
+                  </MenuItem>
+                  <MenuItem onClick={() => handleDelete(category.id)}>
+                    Usuń
+                  </MenuItem>
+                </Menu>
+              </>
+            }
+          />
+          <CardActions disableSpacing sx={{ ml: 1, mb: 1 }}>
+            <Button
+              size="large"
+              variant="outlined"
+              onClick={() => handleCategoryDetailClick(category.id)}
+              sx={{ mr: 2 }}
+            >
+              Przejdź do zestawu
+            </Button>
 
-          <Button size="large" variant="outlined" onClick={handleStartLearning}>
-            Rozpocznij naukę
-          </Button>
-        </CardActions>
-      </Card>
+            <Button
+              size="large"
+              variant="outlined"
+              onClick={() => handleStartLearning(category.id)}
+            >
+              Rozpocznij naukę
+            </Button>
+          </CardActions>
+        </Card>
+      ))}
       <Box sx={{ display: "flex", justifyContent: "center", width: "100%" }}>
         <Button
           startIcon={<AddIcon />}
