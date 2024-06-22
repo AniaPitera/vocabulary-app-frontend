7c92089dc89d56892de06237bbac2dab80144a64
Anna Pitera <annapitera01@gmail.com>
2024-06-22 15:50:50 +0200

    change a path to backend requests

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:9000/API";
+axios.defaults.baseURL = "http://localhost:9000/api";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
