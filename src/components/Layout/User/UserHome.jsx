c299ce8e2810fe3d8dd6a4ea57c6f602dc2e7581
Anna Pitera <annapitera01@gmail.com>
2024-06-23 15:46:51 +0200

    fix user home page

--- src/components/Layout/User/UserHome.jsx
+++ src/components/Layout/User/UserHome.jsx
@@ -1,13 +1,18 @@
-import { Box } from "@mui/material";
+import { Container, Typography, Box } from "@mui/material";
+import Features from "../Default/Features";
+import CategoryList from "../../Category/CategoryList";
 
 const UserHome = () => {
   return (
-    <Box>
-      <div>
-        <h1>Witaj w VocabularyApp</h1>
-        <p>Strona główna po zalogowaniu</p>
-      </div>
-    </Box>
+    <Container maxWidth="lg" sx={{ mt: 10 }}>
+      <Box sx={{ mt: 4, textAlign: "center" }}>
+        <Typography variant="h3">
+          Popraw swoją znajomość słownictwa dzięki naszym interaktywnym fiszkom.
+        </Typography>
+      </Box>
+      <Features />
+      <CategoryList />
+    </Container>
   );
 };
 
