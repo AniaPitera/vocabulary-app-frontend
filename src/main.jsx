691ecbd9c11a6d3a06a1a91d43fd2d8f28c8f382
Anna Pitera <annapitera01@gmail.com>
2024-06-23 20:45:14 +0200

    change path to azure

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import axios from 'axios';
+import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:9000/api";
+axios.defaults.baseURL = "vocabulary-app-backend.azurewebsites.net";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
