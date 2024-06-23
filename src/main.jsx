14abcee5d7ef02c76478eea6faf22c2af1fb8c62
Anna Pitera <annapitera01@gmail.com>
2024-06-23 22:26:56 +0200

    change path

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import axios from "axios";
 
-axios.defaults.baseURL = "vocabulary-app-backend.azurewebsites.net";
+axios.defaults.baseURL = "https://vocabulary-app-backend.azurewebsites.net/api";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
