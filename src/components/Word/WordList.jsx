e019462f023626a95a5b0bb1a379e8bc231c10e7
Anna Pitera <annapitera01@gmail.com>
2024-06-23 15:44:42 +0200

    add words list display, create, edit & delete words

--- src/components/Word/WordList.jsx
+++ src/components/Word/WordList.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
@@ -8,73 +9,40 @@ import DeleteIcon from "@mui/icons-material/DeleteOutlined";
 import CheckIcon from "@mui/icons-material/Check";
 import CancelIcon from "@mui/icons-material/Close";
 import {
-  GridRowModes,
-  DataGrid,
-  GridActionsCellItem,
-  GridRowEditStopReasons,
-} from "@mui/x-data-grid";
+  Card,
+  CardActions,
+  CardContent,
+  TextField,
+  IconButton,
+  Container,
+} from "@mui/material";
+import { jwtDecode } from "jwt-decode";
+import { useParams } from "react-router-dom";
 import { randomId } from "@mui/x-data-grid-generator";
-import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
-import { styled } from "@mui/material/styles";
 
-const initialRows = [
-  {
-    id: randomId(),
-    word: "cat",
-    translation: "kot",
-    isLearned: false,
-    categoryId: randomId(),
-  },
-  {
-    id: randomId(),
-    word: "dog",
-    translation: "pies",
-    isLearned: false,
-    categoryId: randomId(),
-  },
-  {
-    id: randomId(),
-    word: "snake",
-    translation: "wąż",
-    isLearned: false,
-    categoryId: randomId(),
-  },
-  {
-    id: randomId(),
-    word: "rabbit",
-    translation: "królik",
-    isLearned: false,
-    categoryId: randomId(),
-  },
-];
-
-function AddNewWord(props) {
-  const { setRows, setRowModesModel } = props;
-
+const AddNewWordButton = ({ setWords }) => {
   const handleClick = () => {
     const id = randomId();
-    setRows((oldRows) => [
-      ...oldRows,
-      { id, word: "", translation: "", isNew: true },
-    ]);
-    setRowModesModel((oldModel) => ({
-      ...oldModel,
-      [id]: { mode: GridRowModes.Edit, fieldToFocus: "word" },
-    }));
+    const newWord = {
+      id,
+      word: "",
+      translation: "",
+      isLearned: false,
+      isNew: true,
+      isEditMode: true,
+    };
+    setWords((oldWords) => [...oldWords, newWord]);
   };
 
   return (
     <Box
       sx={{
-        mt: 10,
-        mr: 2,
-        ml: 1,
+        mt: 2,
         display: "flex",
-        justifyContent: "space-between",
+        justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Typography variant="h4">Nazwa zestawu</Typography>
       <Button
         startIcon={<AddIcon />}
         onClick={handleClick}
@@ -85,166 +53,246 @@ function AddNewWord(props) {
       </Button>
     </Box>
   );
-}
+};
 
 export default function WordList() {
-  const [rows, setRows] = useState(initialRows);
-  const [rowModesModel, setRowModesModel] = useState({});
+  const { categoryId } = useParams();
+  const [words, setWords] = useState([]);
+  const [categoryName, setCategoryName] = useState("");
 
-  const handleRowEditStop = (params, event) => {
-    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
-      event.defaultMuiPrevented = true;
-    }
-  };
+  useEffect(() => {
+    const fetchCategoryAndWords = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        console.error("No token found");
+        return;
+      }
+      const decodedToken = jwtDecode(token);
+      const userId = decodedToken.user_id;
 
-  const handleEditClick = (id) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
-  };
+      try {
+        const categoryResponse = await axios.get("/category", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          params: { userId: userId },
+        });
 
-  const handleSaveClick = (id) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-  };
-
-  const handleDeleteClick = (id) => () => {
-    setRows(rows.filter((row) => row.id !== id));
-  };
-
-  const handleCancelClick = (id) => () => {
-    setRowModesModel({
-      ...rowModesModel,
-      [id]: { mode: GridRowModes.View, ignoreModifications: true },
-    });
-
-    const editedRow = rows.find((row) => row.id === id);
-    if (editedRow.isNew) {
-      setRows(rows.filter((row) => row.id !== id));
-    }
-  };
-
-  const processRowUpdate = (newRow) => {
-    const updatedRow = { ...newRow, isNew: false };
-    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
-    return updatedRow;
-  };
-
-  const handleRowModesModelChange = (newRowModesModel) => {
-    setRowModesModel(newRowModesModel);
-  };
-
-  const LightTooltip = styled(({ className, ...props }) => (
-    <Tooltip {...props} classes={{ popper: className }} />
-  ))(({ theme }) => ({
-    [`& .${tooltipClasses.tooltip}`]: {
-      backgroundColor: theme.palette.common.white,
-      color: "rgba(0, 0, 0, 0.87)",
-      boxShadow: theme.shadows[1],
-      fontSize: 11,
-    },
-  }));
-
-  const columns = [
-    { field: "word", headerName: "Słówko", width: 500, editable: true },
-    {
-      field: "translate",
-      headerName: "Tłumaczenie",
-      width: 600,
-      editable: true,
-    },
-    {
-      field: "actions",
-      type: "actions",
-      width: 400,
-      cellClassName: "actions",
-      getActions: ({ id }) => {
-        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
-
-        if (isInEditMode) {
-          return [
-            <GridActionsCellItem
-              key={id}
-              icon={
-                <LightTooltip title="Zatwierdź zmiany">
-                  <CheckIcon />
-                </LightTooltip>
-              }
-              label="Save"
-              sx={{
-                color: "primary.main",
-              }}
-              onClick={handleSaveClick(id)}
-            />,
-            <GridActionsCellItem
-              key={id}
-              icon={
-                <LightTooltip title="Anuluj zmiany">
-                  <CancelIcon />
-                </LightTooltip>
-              }
-              label="Cancel"
-              className="textPrimary"
-              onClick={handleCancelClick(id)}
-              color="inherit"
-            />,
-          ];
+        const categoryIdNumber = Number(categoryId);
+        const category = categoryResponse.data.find(
+          (cat) => cat.id === categoryIdNumber
+        );
+        if (category) {
+          setCategoryName(category.name);
+        } else {
+          console.error("Category not found");
         }
 
-        return [
-          <GridActionsCellItem
-            key={id}
-            icon={
-              <LightTooltip title="Edytuj fiszkę">
-                <EditIcon />
-              </LightTooltip>
-            }
-            label="Edit"
-            className="textPrimary"
-            onClick={handleEditClick(id)}
-            color="inherit"
-          />,
-          <GridActionsCellItem
-            key={id}
-            icon={
-              <LightTooltip title="Usuń fiszkę">
-                <DeleteIcon />
-              </LightTooltip>
-            }
-            label="Delete"
-            onClick={handleDeleteClick(id)}
-            color="inherit"
-          />,
-        ];
-      },
-    },
-  ];
+        const wordsResponse = await axios.get("/word", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          params: { categoryId: categoryIdNumber },
+        });
+        setWords(wordsResponse.data);
+      } catch (error) {
+        console.error("Error fetching category or words:", error);
+      }
+    };
+
+    if (categoryId) {
+      fetchCategoryAndWords();
+    }
+  }, [categoryId]);
+
+  const handleEditClick = (id) => {
+    setWords((words) =>
+      words.map((word) =>
+        word.id === id ? { ...word, isEditMode: true } : word
+      )
+    );
+  };
+
+  const handleSaveClick = async (id) => {
+    const word = words.find((word) => word.id === id);
+    if (word.isNew) {
+      await addNewWord(word);
+    } else {
+      await updateWord(word);
+    }
+    setWords((words) =>
+      words.map((word) =>
+        word.id === id ? { ...word, isEditMode: false } : word
+      )
+    );
+  };
+
+  const handleDeleteClick = async (id) => {
+    await deleteWord(id);
+    setWords(words.filter((word) => word.id !== id));
+  };
+
+  const handleCancelClick = (id) => {
+    setWords((words) =>
+      words.map((word) =>
+        word.id === id ? { ...word, isEditMode: false } : word
+      )
+    );
+
+    const editedWord = words.find((word) => word.id === id);
+    if (editedWord.isNew) {
+      setWords(words.filter((word) => word.id !== id));
+    }
+  };
+
+  const handleWordChange = (id, field, value) => {
+    setWords((words) =>
+      words.map((word) => (word.id === id ? { ...word, [field]: value } : word))
+    );
+  };
+
+  const addNewWord = async (newWord) => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await axios.post(
+        "/word",
+        {
+          word: newWord.word,
+          translation: newWord.translation,
+          is_learned: false,
+          category: categoryId,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.status === 201) {
+        setWords((oldWords) =>
+          oldWords.map((word) =>
+            word.id === newWord.id
+              ? { ...newWord, isNew: false, id: response.data.id }
+              : word
+          )
+        );
+      } else {
+        console.error("Error adding word");
+      }
+    } catch (error) {
+      console.error("Error adding word:", error);
+    }
+  };
+
+  const updateWord = async (updatedWord) => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await axios.put(
+        `/word/${updatedWord.id}`,
+        {
+          word: updatedWord.word,
+          translation: updatedWord.translation,
+          is_learned: false,
+          category: categoryId,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.status === 200) {
+        setWords((oldWords) =>
+          oldWords.map((word) =>
+            word.id === updatedWord.id ? { ...word, isNew: false } : word
+          )
+        );
+      } else {
+        console.error("Error updating word");
+      }
+    } catch (error) {
+      console.error("Error updating word:", error);
+    }
+  };
+
+  const deleteWord = async (id) => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await axios.delete(`/word/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.status !== 200) {
+        console.error("Error deleting word");
+      }
+    } catch (error) {
+      console.error("Error deleting word:", error);
+    }
+  };
 
   return (
-    <Box
-      sx={{
-        height: 700,
-        width: "100%",
-        "& .actions": {
-          color: "text.secondary",
-        },
-        "& .textPrimary": {
-          color: "text.primary",
-        },
-      }}
-    >
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        editMode="row"
-        rowModesModel={rowModesModel}
-        onRowModesModelChange={handleRowModesModelChange}
-        onRowEditStop={handleRowEditStop}
-        processRowUpdate={processRowUpdate}
-        slots={{
-          toolbar: AddNewWord,
-        }}
-        slotProps={{
-          toolbar: { setRows, setRowModesModel },
-        }}
-      />
-    </Box>
+    <Container maxWidth="lg">
+      <Typography variant="h4" sx={{ mt: 10 }}>
+        Zestaw - {categoryName}
+      </Typography>
+      {words.map((word) => (
+        <Card key={word.id} sx={{ marginTop: 2 }}>
+          <CardContent>
+            {word.isEditMode ? (
+              <>
+                <TextField
+                  label="Słówko"
+                  value={word.word}
+                  onChange={(e) =>
+                    handleWordChange(word.id, "word", e.target.value)
+                  }
+                  fullWidth
+                />
+                <TextField
+                  label="Tłumaczenie"
+                  value={word.translation}
+                  onChange={(e) =>
+                    handleWordChange(word.id, "translation", e.target.value)
+                  }
+                  fullWidth
+                  sx={{ marginTop: 2 }}
+                />
+              </>
+            ) : (
+              <>
+                <Typography variant="h6">{word.word}</Typography>
+                <Typography variant="body2">{word.translation}</Typography>
+              </>
+            )}
+          </CardContent>
+          <CardActions>
+            {word.isEditMode ? (
+              <>
+                <IconButton onClick={() => handleSaveClick(word.id)}>
+                  <CheckIcon />
+                </IconButton>
+                <IconButton onClick={() => handleCancelClick(word.id)}>
+                  <CancelIcon />
+                </IconButton>
+              </>
+            ) : (
+              <>
+                <IconButton onClick={() => handleEditClick(word.id)}>
+                  <EditIcon />
+                </IconButton>
+                <IconButton onClick={() => handleDeleteClick(word.id)}>
+                  <DeleteIcon />
+                </IconButton>
+              </>
+            )}
+          </CardActions>
+        </Card>
+      ))}
+      <AddNewWordButton setWords={setWords} />
+    </Container>
   );
 }
